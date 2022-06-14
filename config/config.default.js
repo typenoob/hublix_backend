5725cef8da107d5c34c8c818b1e75dc039b1ac4f
coyote <chenyutao0706@gmail.com>
2022-06-14 21:01:48 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,66 @@
+/* eslint valid-jsdoc: "off" */
+
+"use strict";
+const path = require("path");
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = (appInfo) => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1653123705435_9404";
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+  config.jwt = {
+    secret: "GmvjBd1gxK89iZDfcc1M", //自定义 token 的加密条件字符串
+  };
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: ["http://localhost:8080"], //允许访问接口的白名单
+  };
+  config.cors = {
+    origin: "*",
+    allowMethods: "GET,HEAD,PUT,POST,DELETE,PATCH",
+  };
+  config.mysql = {
+    client: {
+      // host
+      host: "192.168.157.128",
+      // 端口号
+      port: "3306",
+      // 用户名
+      user: "root",
+      // 密码
+      password: "root",
+      // 数据库名
+      database: "hublix",
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+  config.multipart = {
+    mode: "file",
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
